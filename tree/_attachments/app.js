8538caa5a0744a390ba5727cb53f0fd8751b1e13
Noah Zoschke <noah@heroku.com>
2010-02-06 10:14:42 -0800

    cool!

--- tree/_attachments/app.js
+++ tree/_attachments/app.js
@@ -3,11 +3,10 @@
 //    data[i] = 'item #' + (i+1);
 //};
 
-var Node = function(path) {
-    this.path = path;
+var Node = function() {
 }
 Node.prototype = {
-  toString: function() { return this.path }
+  toString: function() { return this.label + (this.numChildren > 0 ? ' > ' : ''); }
 }
 var data = [new Node('abc'), {a: 'a', toString: function() { return 'ass'}}];
 
@@ -24,18 +23,15 @@ uki({
       view: 'ScrollPane', rect: '200 200', anchors: 'left top bottom', background: '#F00',
       scrollableH: 'false', scrollableV: 'true',
       childViews: [{
-        view: 'Label', rect: '200 200',  // label with long text
-        anchors: 'top left rigth', // anchored to the top right (grow down)
-        multiline: 'true',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        inset: '3 3', id: 'tree1'
+        view: 'List', rect: '0 0 200 210', anchors: 'top left right', 
+        data: [], rowHeight: 30, id: 'list1', throttle: 0
       }]
     }, {
       view: 'ScrollPane', rect: '200 0 200 200', anchors: 'left top bottom', background: '#0F0',
       scrollableH: 'false', scrollableV: 'true',
       childViews: [{
-        view: 'List', rect: '0 0 200 900000', anchors: 'top left right', 
-        data: [], rowHeight: 30, id: 'list', throttle: 0
+        view: 'List', rect: '0 0 200 210', anchors: 'top left right', 
+        data: [], rowHeight: 30, id: 'list2', throttle: 0
       }]
     }],
     bottomChildViews: []
@@ -49,8 +45,9 @@ uki('#doIt').click(function () {
         
 });
 
-uki('#list').click(function() {
-  alert(uki('#list').selectedIndex());
+uki('#list1').click(function() {
+  var selectedNode = this.data()[this.selectedIndex()];
+  getChildren(selectedNode.key, '#list2');
 })
  
 uki('#doIt').resizeToContents('width').layout(); // fix button size
@@ -60,47 +57,32 @@ uki('#doIt').resizeToContents('width').layout(); // fix button size
 .html(uki('#target').html() + ' Lorem ipsum dolor sit amet, consectetur adipisicing elit ') // add more text
         .resizeToContents('height').parent().resizeToContents('height').layout(); // resize to contents and relayout
 */
-$.CouchApp(function(app) {
-  return;
-	// http://localhost:5984/media/_design/tree/_view/children?group=true&startkey=[["ROOT"]]&endkey=[["ROOT"],[{}]]
-	// {"rows":[
-	// {"key":[["ROOT"],["jason"]],"value":15690},
-	// {"key":[["ROOT"],["noah"]],"value":10360}
-	// ]}
-	
-	function getChildren(path) {
-	  return;
-		path = path.split('/');
+function getChildren(path, listID) {
+  $.CouchApp(function(app) {
+  	// http://localhost:5984/media/_design/tree/_view/children?group=true&startkey=[["ROOT"]]&endkey=[["ROOT"],[{}]]
+  	// {"rows":[
+  	// {"key":[["ROOT"],["jason"]],"value":15690},
+  	// {"key":[["ROOT"],["noah"]],"value":10360}
+  	// ]}
+
 		app.view("children", {
 			group:		true, 
 			startkey: 	[path],
 			endkey: 	[path,[{}]],
 			success:	function(json) {
-				// add links to current and parent path
-				var firstChildPath = json.rows[0].key[0].join('/') + '/' + json.rows[0].key[1].join('/');
-				var parentPath = firstChildPath.split('/').slice(0,-2).join('/');
-				var currentPath = firstChildPath.split('/').slice(0,-1).join('/');
-				$("#path").html(currentPath);
-				$("#fileList").html('');
-				$("#fileList").append("<li><a class=\"node\" href=\"" + currentPath + "\">.</a></li>");
-				$("#fileList").append("<li><a class=\"node\" href=\"" + parentPath + "\">..</a></li>");
-				
-				// add links for children
+			  var data = [];
 				for (var i in json.rows) { // FIXME: for each?!
-					var row = json.rows[i];
-					var childPath = row.key[0].join('/') + '/' + row.key[1].join('/');
-					var row = json.rows[i];
-					$("#fileList").append("<li><a class=\"node\" href=\"" + childPath + "\">" + row.key[1].join('/') + "</a></li>");
+				  var row = json.rows[i];
+				  var node = new Node();
+				  node.label = row.key[1][0];
+				  node.key = row.key[0];
+				  node.key.push(node.label);
+				  node.numChildren = row.value;
+				  data[i] = node;
 				}
+				uki(listID).data(data);
 			}
 		});
-		
-	}
-	
-	$(".node").live("click", function() {
-		getChildren($(this).attr('href')); // get raw (relative) href attr
-		return false;
-	});
-	
-	getChildren("ROOT");
-});
+  });  
+}
+getChildren(["ROOT"], "#list1");
