a2dbccf7c7047ca32d73a0be4814463e5ef0ae7b
Noah Zoschke <noah@heroku.com>
2010-02-07 11:56:18 -0800

    store metadata both on field and default

--- search/fulltext/by_metadata/index.js
+++ search/fulltext/by_metadata/index.js
@@ -1,7 +1,19 @@
 function(doc) {
 	var ret = new Document();
-	if (doc.Name)   ret.add(doc.Name, {'field': 'Name'});
-	if (doc.Artist) ret.add(doc.Artist, {'field': 'Artist'});
-	if (doc.Album)  ret.add(doc.Album, {'field': 'Album'});
+	if (doc.Name) {
+	  ret.add(doc.Name);
+	  ret.add(doc.Name, {'field': 'Name'});
+  }
+	if (doc.Artist) {
+	  ret.add(doc.Artist);
+	  ret.add(doc.Artist, {'field': 'Artist'});
+  }
+	if (doc.Album) {
+	  ret.add(doc.Album);
+	  ret.add(doc.Album, {'field': 'Album'});
+  }
+  if (doc.owner) {
+	  ret.add(doc.Album, {'field': 'Owner'});
+  }
 	return ret;
 }
