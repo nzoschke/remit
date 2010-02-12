2a779be63396dd68fe393b9b51651c10a1a03798
Noah Zoschke <noah@heroku.com>
2010-02-12 00:21:40 -0800

    change to run on monkeydishwasher

--- tree/views/by_network_location/map.js
+++ tree/views/by_network_location/map.js
@@ -1,7 +1,7 @@
 function(doc) {
   if (doc.Location) {
     var l = doc.Location;
-    l = l.replace("file://localhost/Network/NAS/", "http://noah:88cos69X@hero2000.dyndns.org/");
+    l = l.replace("file://localhost/Network/NAS/", "http://hero2000.dyndns.org/");
     emit(doc._id, l);
   }
 }
